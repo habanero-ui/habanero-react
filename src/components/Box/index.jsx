06f882a140ef628966aea4e6e33e5e1064ba0310
Nick Johnson <nickjohnson.dev@gmail.com>
2020-11-10 09:20:38 -0600

    Add spacing prop validation to Box

--- src/components/Box/index.jsx
+++ src/components/Box/index.jsx
@@ -1,18 +1,55 @@
+import every from 'lodash/every'
+import forEach from 'lodash/forEach'
+import includes from 'lodash/includes'
+import isArray from 'lodash/isArray'
 import isNaN from 'lodash/isNaN'
+import isNil from 'lodash/isNil'
 import isNumber from 'lodash/isNumber'
+import map from 'lodash/map'
+import without from 'lodash/without'
+import { readableColor } from 'polished'
 import PropTypes from 'prop-types'
 import React from 'react'
 import styled from 'styled-components'
 
 import borderRadii from '../../constants/borderRadii'
 import colors from '../../constants/colors'
+import spacingAliases from '../../constants/spacingAliases'
 import getResponsivePropValue from '../../helpers/getResponsivePropValue'
 import useScreenSizeType from '../../helpers/useScreenSizeType'
 
-const Root = styled.div((props) => ({
-  backgroundColor: props.theme.colors[props.backgroundColor],
-  borderRadius: props.theme.borderRadii[props.borderRadius],
-}))
+const Root = styled.div((props) => {
+  const backgroundColor = props.theme.colors[props.backgroundColor]
+
+  return {
+    backgroundColor,
+    borderRadius: props.theme.borderRadii[props.borderRadius],
+    ...(props.showInteractionOverlay
+      ? {
+          cursor: 'pointer',
+          position: 'relative',
+          '&::after': {
+            backgroundColor: readableColor(backgroundColor || 'white'),
+            bottom: 0,
+            content: '""',
+            left: 0,
+            pointerEvents: 'none',
+            opacity: 0,
+            position: 'absolute',
+            right: 0,
+            top: 0,
+            transition: 'opacity 100ms ease-in-out',
+          },
+          '&:hover::after': {
+            opacity: readableColor(backgroundColor || 'white', '0.1', '0.2'),
+          },
+          '&:active::after': {
+            opacity: readableColor(backgroundColor || 'white', '0.25', '0.4'),
+          },
+        }
+      : {}),
+  }
+})
 
 const spacingPropType = PropTypes.oneOfType([
   PropTypes.number,
@@ -103,6 +140,43 @@ export default function Box(props) {
     styleProp,
   ])
 
+  React.useEffect(() => {
+    forEach(
+      {
+        margin,
+        marginBottom,
+        marginLeft,
+        marginRight,
+        marginTop,
+        marginX,
+        marginY,
+        padding,
+        paddingBottom,
+        paddingLeft,
+        paddingRight,
+        paddingTop,
+        paddingX,
+        paddingY,
+      },
+      (value, key) => getIsSpacingPropValid(key)(value),
+    )
+  }, [
+    margin,
+    marginBottom,
+    marginLeft,
+    marginRight,
+    marginTop,
+    marginX,
+    marginY,
+    padding,
+    paddingBottom,
+    paddingLeft,
+    paddingRight,
+    paddingTop,
+    paddingX,
+    paddingY,
+  ])
+
   return (
     <Root
       as={component}
@@ -114,6 +188,38 @@ export default function Box(props) {
       {children}
     </Root>
   )
+}
+
+function getIsSpacingPropValid(propName) {
+  return (value) => {
+    if (isNil(value)) return
+
+    const negativeSpacingAliases = map(
+      without(spacingAliases, 'none'),
+      (alias) => `-${alias}`,
+    )
+    const isSingleValueValid = (singleValue) =>
+      includes(
+        ['', ...spacingAliases, ...negativeSpacingAliases],
+        singleValue,
+      ) || !isNaN(parseFloat(singleValue))
+
+    const isValid = isArray(value)
+      ? value.length >= 1 &&
+        value.length <= 3 &&
+        every(value, isSingleValueValid)
+      : isSingleValueValid(value)
+
+    if (!isValid) {
+      // eslint-disable-next-line no-console
+      console.error(
+        `Box: Bad value "${value}". The "${propName}" prop must be a number to be multiplied by 4, an array of two or three numbers corresponding to screen sizes, one of the following aliases, or an array of two or three of the following aliases, corresponding to screen sizes:`,
+        String([...spacingAliases, ...negativeSpacingAliases]),
+      )
+    }
+
+    return isValid
+  }
 }
 
 export function getRemFromSpacing(spacing) {
